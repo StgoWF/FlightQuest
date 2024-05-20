04d0b1d20f01aee797c50d3ee2665a64ed7ccb63
Telmen Gantulga <telme9595@gmail.com>
2024-05-20 14:16:58 -0700

    Revert "Merge branch 'main' of github.com:StgoWF/FlightQuest"
    This reverts commit c85967077f0d7122f631a309c6a65e48082f3a65, reversing
    changes made to 3539b5ae352590a1ec48fd22646b7ae1dcc374c7.

--- models/index.js
+++ models/index.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 const config = require('../config/config');
 const UserModel = require('./user');
 const TripModel = require('./trip');
-const env = process.env.NODE_ENV || 'production';
+const env = process.env.NODE_ENV || 'development';
 const dbConfig = config[env];
 let sequelize;
 
